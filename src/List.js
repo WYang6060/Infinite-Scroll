21140f3dfe29e34bc7e43ca106155ff796a1f7c1
Wesley Yang <57732129+WYang6060@users.noreply.github.com>
2021-08-17 02:42:22 -0400

    add content loader

--- src/List.js
+++ src/List.js
@@ -2,6 +2,22 @@ import { useCallback, useEffect, useState } from "react";
 import ListGroup from "./components/ListGroup/ListGroup";
 import useInfiniteScroll from "./hook/useInfiniteScroll";
 
+import ContentLoader from "react-content-loader";
+
+const ThreeDots = (props) => (
+  <ContentLoader
+    viewBox="0 0 400 40"
+    height={50}
+    width={400}
+    backgroundColor="transparent"
+    {...props}
+  >
+    <circle cx="150" cy="16" r="8" />
+    <circle cx="194" cy="16" r="8" />
+    <circle cx="238" cy="16" r="8" />
+  </ContentLoader>
+);
+
 const List = () => {
   const [listItems, setListItems] = useState(
     Array.from(Array(15).keys(), (n) => n + 1)
@@ -28,7 +44,7 @@ const List = () => {
   return (
     <>
       <ListGroup listItems={listItems} />
-      {isFetching && "Fetching more list items..."}
+      {isFetching && <ThreeDots />}
     </>
   );
 };
--- src/components/ListGroup/ListGroup.js
+++ src/components/ListGroup/ListGroup.js
@@ -3,7 +3,9 @@ import "./ListGroup.css";
 const ListGroup = ({ listItems }) => (
   <ul className="list-group">
     {listItems.map((val) => (
-      <li className="list-group-item">List Item {val}</li>
+      <li key={val} className="list-group-item">
+        List Item {val}
+      </li>
     ))}
   </ul>
 );
